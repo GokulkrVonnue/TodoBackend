bfe33dc34787429fc64fdcf55b70cc698914229d
GokulkrVonnue <gokulkr@vonnue.com>
2024-09-12 09:34:28 +0530

    db added

--- app.js
+++ app.js
@@ -8,8 +8,58 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(express.json())
 
-app.get('/', (req,res) => {
+app.get('/todo', (req,res) => {
   db.all("SELECT * FROM todo", [],(err,rows)=>{
+    if(err){
+      throw err
+    }
+    res.json(rows);
+    
+  })
+  
+})
+app.get('/filter',(req,res) => {
+  db.all("SELECT * FROM filterdata", [],(err,rows)=>{
+    if(err){
+      throw err
+    }
+    res.json(rows);
+    
+  })
+});
+app.get('/filter/:id',(req,res) => {
+  db.all(`SELECT * FROM filterdata WHERE id=?`, [req.params.id],(err,rows)=>{
+    if(err){
+      throw err 
+    }
+    res.json(rows);
+  })                                                  
+});
+app.post('/filter',(req,res) => {
+  const {filterName,queryName}=req.body;
+  console.log(req.body)
+  db.run(
+    "INSERT INTO filterdata (filterName,filterquery) VALUES (?,?)",
+    [filterName,queryName],
+    function(err){
+      if(err){
+        console.log(err)
+      }
+      console.log(req.body)
+      res.json({id: this.lastID,filterName,queryName})
+    }
+  )
+});
+app.delete('/filter/:id',(req,res)=>{
+  db.run(`DELETE FROM filterdata WHERE id=?`,req.params.id,function(err){
+    if(err){
+      return console.error(err.message)
+    }
+    res.send("Deleted")
+  })
+})
+app.get('/todo/:id', (req,res) => {
+  db.all(`SELECT * FROM todo WHERE id=?`, [req.params.id],(err,rows)=>{
     if(err){
       throw err
     }
@@ -18,7 +68,13 @@ app.get('/', (req,res) => {
   
 })
 
-app.post('/',(req,res) => {
+// app.get('/filter',(req,res)=>{
+//   console.log(req.body)
+//   res.send("hi")
+
+// })
+
+app.post('/todo',(req,res) => {
   const {taskName,description,date}=req.body;
   console.log(req.body)
   db.run(
@@ -39,7 +95,7 @@ app.post('/',(req,res) => {
 
 })
 
-app.delete('/:id',(req,res)=>{
+app.delete('/todo/:id',(req,res)=>{
   db.run(`DELETE FROM todo WHERE id=?`,req.params.id,function(err){
     if(err){
       return console.error(err.message)
@@ -47,7 +103,46 @@ app.delete('/:id',(req,res)=>{
     res.send("Deleted")
   })
 })
+app.put('/todo/:id',(req,res)=>{
+
+  const { taskName, description, date } = req.body
+  const params=[taskName,description,date,req.params.id]
+  db.run(`UPDATE todo SET taskName = ?, description = ?, date = ? WHERE id = ?`,params,function(err){
+    if(err){
+      console.log(err)
+    }
+    console.log(req.body)
+    res.json({id: this.lastID,taskName,description,date})
+  });
+  
+})
+app.get("/labels",(req,res)=>{
+  db.all(`SELECT * FROM labeldata WHERE id=?`, [req.params.id],(err,rows)=>{
+    if(err){
+      throw err
+    }
+    res.json(rows);
+  })
+
+})
+
+app.post('/labels',(req,res) => {
+  const {LabelName}=req.body;
+  console.log(req.body)
+  db.run(
+    "INSERT INTO labeldata (LabelName) VALUES (?)",
+    [LabelName],
+    function(err){
+      if(err){
+        console.log(err)
+      }
+      console.log(req.body)
+      res.json({id: this.lastID,LabelName})
+    }
+  )
+});
 
 app.listen(port,() => {
   console.log(`Server is listeing on port ${port} `)
 })
+            
--- db.js
+++ db.js
@@ -4,5 +4,11 @@ const db=new sqlite.Database('./database.db')
 db.serialize(()=>{
     db.run("CREATE TABLE IF NOT EXISTS todo (id INTEGER PRIMARY KEY AUTOINCREMENT, taskName TEXT, description TEXT, date Text)")
 });
+db.serialize(()=>{
+    db.run("CREATE TABLE IF NOT EXISTS filterdata (id INTEGER PRIMARY KEY AUTOINCREMENT, filterName TEXT, filterquery TEXT)")
+});
+db.serialize(()=>{
+    db.run("CREATE TABLE IF NOT EXISTS labeldata (id INTEGER PRIMARY KEY AUTOINCREMENT, LabelName TEXT)")
+});
 
 module.exports=db;
